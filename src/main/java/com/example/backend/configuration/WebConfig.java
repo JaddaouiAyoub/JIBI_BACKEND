b1c72ccf1045d3ab62a4190a340af348b0e644af
Jaddaoui Ayoub <Jaddaoui.salim@gmail.com>
2024-06-14 02:25:46 +0200

    commit deploiement

--- src/main/java/com/example/backend/configuration/WebConfig.java
+++ src/main/java/com/example/backend/configuration/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://jibi-1efntzpgg-jaddaoui-ayoubs-projects.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
